54a9813454d6e40dad85c38ade6922e402b204cc
everett2002 <everettho2002@gmail.com>
2023-02-22 14:42:35 -0800

    preventing refresh for links to own page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import NavbarElementCollapsible from './navbarelementcollapsible';
 import NavbarElement from './navbarelement';
 
-const navbar = ({showCollapsible}) => {
+const navbar = ({ showCollapsible }) => {
     return (
         <nav className='navbar'>
-            <NavbarElement title='About' link='./App.css#about' />
-            <NavbarElement title='Projects' link='./App.css#projects' />
+            <NavbarElement title='About' link='#about' />
+            <NavbarElement title='Projects' link='#projects' />
             <NavbarElementCollapsible title={<i class="fa-solid fa-bars fa-2x"></i>} showCollapsible={showCollapsible} />
             {/* <NavbarElement title={<i class="fa-solid fa-circle-half-stroke fa-2x"></i>} /> */}
-            
-            
+
+
         </nav>
     )
 }

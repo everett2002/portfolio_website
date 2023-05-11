9cf7363cf4fdf3efc030917da197ee06baf90157
everett2002 <83052738+everett2002@users.noreply.github.com>
2023-05-10 22:07:12 -0700

    Update Youtube Link

--- src/components/maincard.jsx
+++ src/components/maincard.jsx
@@ -9,7 +9,7 @@ const maincard = () => {
             <div className='maincard-options'>
                 <a href="https://www.linkedin.com/in/everett-ho-ab97b219b/" className='maincard-link'><i class="fa-brands fa-linkedin fa-2x"></i></a>
                 <a href="mailto:everettho2002@gmail.com" className='maincard-link'><i class="fa-solid fa-envelope fa-2x"></i></a>
-                <a href="" className='maincard-link'><i class="fa-brands fa-youtube fa-2x"></i></a>
+                <a href="https://youtube.com/@everettho7467" className='maincard-link'><i class="fa-brands fa-youtube fa-2x"></i></a>
                 <a href="https://github.com/everett2002?tab=repositories" className='maincard-link'><i class="fa-brands fa-github fa-2x"></i></a>
             </div>
         </div>
